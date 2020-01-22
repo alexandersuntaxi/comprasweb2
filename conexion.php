70af640340768e038995f0f67129e7d2d43c563b
alexandersuntaxi <59911348+alexandersuntaxi@users.noreply.github.com>
2020-01-22 17:22:18 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 /* Conexión BD */
-define('DB_SERVER', 'comprasweb-23-6-95q5m');
+define('DB_SERVER', 'ip-10-0-161-234.ec2.internal');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
 define('DB_DATABASE', 'COMPRASWEB');
