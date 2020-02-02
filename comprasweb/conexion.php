e210efd802a3bcc09f2d451fc23994618427f5d9
alexandersuntaxi <59911348+alexandersuntaxi@users.noreply.github.com>
2020-02-02 15:01:41 +0100

    Update conexion.php

--- comprasweb/conexion.php
+++ comprasweb/conexion.php
@@ -1,9 +1,9 @@
 <?php
 /* Conexión BD */
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '10.128.26.16');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'comprasweb');
+define('DB_PASSWORD', 'rootroot');
+define('DB_DATABASE', 'COMPRASWEB');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
